628a2e67abf2f441c53d89492fd070bcac50dcb7
Danik <danik.cyrkunov@gmail.com>
2014-08-10 16:15:44 +0300

    Login Azaru Password 123

--- main.go
+++ main.go
@@ -3,10 +3,38 @@ package main
 import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+	"net/http"
 )
+
+func checkPassword(login, password string) bool {
+	if login == "azaru" && password == "123"{
+		return true
+	}
+	return false
+}
 
 func indexHandler(rnd render.Render) {
 	rnd.HTML(200,"index",nil)
+}
+
+func mainHandler(rnd render.Render, r *http.Request ) {
+
+	rnd.HTML(200,"main",nil)
+}
+
+func loginHandler(rnd render.Render, r *http.Request ) {
+	login := r.FormValue("login")
+	pass := r.FormValue("password")
+	if checkPassword(login,pass){
+		rnd.Redirect("/main")
+	}else{
+		rnd.Redirect("/error_Login")
+	}
+}
+
+
+func error_LoginHandler(rnd render.Render) {
+	rnd.HTML(200,"error","Didn't find user")
 }
 
 func main() {
@@ -24,7 +52,15 @@ func main() {
 	m.Use(martini.Static("assets", staticOptions))
 
 	m.Get("/",indexHandler)
+	m.Get("/main",mainHandler)
+	m.Post("/login",loginHandler)
+	m.Get("/error_Login",error_LoginHandler)
 	m.Run()
 }
+
+
+
+
+
 
 
